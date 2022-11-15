828fa22429df435af2063f0e1f73e94beae6e3d2
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2022-11-15 22:12:49 +0900

    deploy

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import "./index.scss";
 import App from "./App";
 import AuthProvider from "./context/Auth";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
